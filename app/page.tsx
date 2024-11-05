60fcb9f7d4938c646d8cbad60ed88acc5829c285
Chung Wei <leongchungwei@hotmail.com>
2024-11-05 22:56:46 +0800

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
+import { problemAction } from "./_actions";
+
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null;
+  return (
+    <form action={problemAction.bind({ name: "test" })}>
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
